760c6deb9aa146343106099a4b3a8c3f0da2914c
AndreaParrini <andrea.parrini26@gmail.com>
2024-01-12 20:54:27 +0100

    create array random number and push in all number

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Ciao');
+const randomNumbers = [];
+
+while( randomNumbers.length < 5 ){
+
+    const number = Math.floor(Math.random() * 101);
+    randomNumbers.includes(number) ? '' : randomNumbers.push(number);
+
+}
+
+const rowEl = document.querySelector('.row');
+
+randomNumbers.forEach(number => {
+    
+    const colMarkUp = `
+    <div class="col p-4 border border-black text-center fs-5"> ${number}</div>
+    `;
+    rowEl.insertAdjacentHTML('beforeend', colMarkUp)
+
+
+});
